e6611cc1c2280eab36464f275a4403b7c23bacc9
sbx_user1051 <sbx_user1051@169.254.84.133>
2025-09-21 21:16:51 +0000

    Sun Sep 21 21:16:51 UTC 2025

--- test/CHK/checkout_test.rb
+++ test/CHK/checkout_test.rb
@@ -29,5 +29,7 @@ class ClientTest < Minitest::Test
 
   def test_checkout_mixed_offers
     assert_equal 175, CheckoutSolution.new.checkout('AAABB'), 'Should return 175 for AAABB'
+    assert_equal 290, CheckoutSolution.new.checkout('AAAABBCDD'), 'Should return 290 for AAAABBCDD'
   end
 end
+
