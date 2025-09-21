a22843c0b2d8dd03465fcc96781a4a510d83e183
sbx_user1051 <sbx_user1051@169.254.58.97>
2025-09-21 18:52:36 +0000

    Sun Sep 21 18:52:36 UTC 2025

--- lib/solutions/CHK/checkout_solution.rb
+++ lib/solutions/CHK/checkout_solution.rb
@@ -1,8 +1,15 @@
 # noinspection RubyUnusedLocalVariable
 class CheckoutSolution
+  PRICES = {
+    'A' => 50,
+    'B' => 30,
+    'C' => 20,
+    'D' => 15
+  }
 
   def checkout(skus)
     raise 'Not implemented'
   end
 
 end
+
--- test/CHK/checkout_test.rb
+++ test/CHK/checkout_test.rb
@@ -9,7 +9,11 @@ require_solution 'CHK'
 class ClientTest < Minitest::Test
 
   def test_checkout_individual_items
-
+    assert_equal 50, CheckoutSolution.new.checkout('A'), 'Should return 50 for A'
+    assert_equal 30, CheckoutSolution.new.checkout('B'), 'Should return 30 for B'
+    assert_equal 20, CheckoutSolution.new.checkout('C'), 'Should return 20 for C'
+    assert_equal 15, CheckoutSolution.new.checkout('D'), 'Should return 15 for D'
   end
 
 end
+
