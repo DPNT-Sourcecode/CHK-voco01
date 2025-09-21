916f835ae0bd699a8c50db7a225ea6f053ade2c5
sbx_user1051 <sbx_user1051@169.254.84.133>
2025-09-21 22:39:51 +0000

    Sun Sep 21 22:39:51 UTC 2025

--- lib/solutions/CHK/checkout_solution.rb
+++ lib/solutions/CHK/checkout_solution.rb
@@ -65,37 +65,10 @@ class CheckoutSolution
 
     total_price = 0
 
+    _apply_free_item_offers(item_counts)
+    total_price += _apply_group_offers(item_counts)
+    total_price += _apply_quantity_offers(item_counts)
     
-
-    group_items = GROUP_OFFERS[:items]
-    group_offer_count = GROUP_OFFERS[:count]
-    group_offer_price = GROUP_OFFERS[:price]
-
-    eligible_group_items = item_counts.select  { |item, count| group_items
-                                      .include?(item) && count > 0}
-                                      .flat_map{ |item, count| Array.new(count, item)}.sort_by{|item| PRICES[item]}
-
-    while eligible_group_items.length >= group_offer_count
-      items_for_offer = eligible_group_items.pop(group_offer_count)
-      total_price += group_offer_price
-      items_for_offer.each { |item| item_counts[item] -= 1}
-    end
-    
-
-    OFFERS.each do |item_sku, offers|
-      sorted_offers = offers.sort_by { |offer| -offer[:count] }
-
-      sorted_offers.each do |offer_details|
-        offer_count = offer_details[:count]
-        offer_price = offer_details[:price]
-
-        while item_counts[item_sku] >= offer_count
-          total_price += offer_price
-          item_counts[item_sku] -= offer_count
-        end
-      end
-    end
-
     item_counts.each do |item, count|
       total_price += PRICES[item] * count
     end
@@ -106,7 +79,6 @@ class CheckoutSolution
   private
 
   def _apply_free_item_offers(item_counts)
-    current_offer_price = 0
     FREE_ITEM_OFFERS.each do |item_sku, offer_details|
       offer_count = offer_details[:count]
       free_item_sku = offer_details[:free_item]
@@ -120,10 +92,45 @@ class CheckoutSolution
       end
       item_counts[free_item_sku] = [0, item_counts[free_item_sku]].max
     end
+  end
 
+  def _apply_group_offers(item_counts)
+    current_offer_price = 0
+    group_items = GROUP_OFFERS[:items]
+    group_offer_count = GROUP_OFFERS[:count]
+    group_offer_price = GROUP_OFFERS[:price]
+
+    eligible_group_items = item_counts.select  { |item, count| group_items
+                                      .include?(item) && count > 0}
+                                      .flat_map{ |item, count| Array.new(count, item)}.sort_by{|item| PRICES[item]}
+
+    while eligible_group_items.length >= group_offer_count
+      items_for_offer = eligible_group_items.pop(group_offer_count)
+      current_offer_price += group_offer_price
+      items_for_offer.each { |item| item_counts[item] -= 1}
+    end
+    current_offer_price
+  end
+
+  def _apply_quantity_offers(item_counts)
+    current_offer_price = 0
+    OFFERS.each do |item_sku, offers|
+      sorted_offers = offers.sort_by { |offer| -offer[:count] }
+
+      sorted_offers.each do |offer_details|
+        offer_count = offer_details[:count]
+        offer_price = offer_details[:price]
+
+        while item_counts[item_sku] >= offer_count
+          current_offer_price += offer_price
+          item_counts[item_sku] -= offer_count
+        end
+      end
+    end
     current_offer_price
   end
 end
+
 
 
 
